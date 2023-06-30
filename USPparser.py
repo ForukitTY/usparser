3b0d422a42a198503e57d3e1e24b42726ace5746
GedgafovMukhamed <mukhamed.gedgafov@axxonsoft.dev>
2023-06-30 13:28:42 +0300

    1.3.8 сделал вывод месседжа в тг чуть понятней

--- USPparser.py
+++ USPparser.py
@@ -1,24 +1,35 @@
 from bs4 import BeautifulSoup
-# TODO: жирный шрифт и чуть более понятное выделение текста
+# TODO: оценку на экзаменах
+# TODO: если студен восстановившийся, бот работает некорректно
 
 
 def sem_parser(req, semestr=0):
     soup = BeautifulSoup(req, 'html.parser')
 
     try:
-        table = soup.find_all(attrs={"class": "clTableBold"})[semestr-1]
+        tables = soup.find_all(attrs={"class": "clTableBold"})
+        semestr_count_by_user = len(tables)
+        table = tables[semestr-1]
     except:
         return 'Такого семестра нет'
     PREDMET_ID = 0
-    PREDMET = 1
+    PREDMET_NAMES = 1
     PREPOD_ID = 2
     ITOG_ID = -2
     parsed_line = ''
     for row in table.find_all('tr')[2:]:
         cells = row.find_all('td')
         list_control_points = row.find_all(attrs={"class": "cltdb"})[:3]
-        line_mark = cells[ITOG_ID].text.strip()
-        parsed_line += cells[PREDMET_ID].text.strip() + '. ' + cells[PREDMET].text.strip() + ' - ' + cells[PREPOD_ID].text.strip() \
-                       + ' | \n' + str([int(control_mark_i.text) for control_mark_i in list_control_points]) + ' Итог: ' + line_mark + ('  ✅\n\n' if int(line_mark) > 60 else '  🛑\n\n')
+        predmet_number_iter = cells[PREDMET_ID].text.strip()
+        ekzamen_name_type_list = cells[PREDMET_NAMES].text.strip().split('\n')
+        predmet_name = ekzamen_name_type_list[0]
+        ekzamen_type = ekzamen_name_type_list[1] if len(ekzamen_name_type_list) > 1 else ""
+        prepod_name = cells[PREPOD_ID].text.strip()
+        itog_mark = cells[ITOG_ID].text.strip()
+
+        parsed_line += predmet_number_iter + '. ' + predmet_name + '\n' +\
+                       ekzamen_type + prepod_name + '\n' +\
+                       str([int(control_mark_i.text) for control_mark_i in list_control_points]) + \
+                       ' Итог: ' + itog_mark + ('  ✅\n\n' if int(itog_mark) > 60 else '  🛑\n\n')
 
     return parsed_line
